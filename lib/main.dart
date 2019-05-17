ceae56fb530b9327649d8edbf7a7b500ba63687b
Simen Daehlin <simen.dehlin@gmail.com>
2019-05-17 18:25:18 +0100

    Added images

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('The 🎲 Game')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +18,17 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset('images/dice1.png')
+          ),
+          Expanded(
+            child: Image.asset('images/dice2.png')
+          ),
+        ],
+      ),
+    );
   }
 }
